9c11801ddd46b14f2b466a1f447e85342df8daaf
huatree <wang.yaohua@foxmail.com>
2022-11-05 23:06:22 +0800

    ✨ feat(utils): 优化接口请求封装函数

--- utils/http.js
+++ utils/http.js
@@ -3,13 +3,13 @@ import apiConfig from '../api/index'
 const BASE_URL = 'http://123.207.32.32:9001'
 
 class HTRequest {
-    run(serviceName, originalParams, options = {}) {
+    run(serviceName, originalParams = {}, options = {}) {
         return new Promise((resolve, reject) => {
             const service = apiConfig[serviceName]
             if (service.isMock) {
                 try {
                     const result = require(`../mock/${serviceName}.js`) // 返回的是 module.exports 对象， 默认为 {}
-                    console.log(`${serviceName}获取mock数据: ${JSON.stringify(result)}`);
+                    console.log(`${serviceName}获取mock数据: ${JSON.stringify(result)}`)
                     resolve(result)
                 } catch (error) {
                     console.error(`${serviceName}模拟数据不存在`)
