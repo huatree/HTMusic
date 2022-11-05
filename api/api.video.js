a30d5f0a656fd26091ede340ca946e8d9240c79d
huatree <wang.yaohua@foxmail.com>
2022-11-05 12:39:07 +0800

    ✨ feat(video): 新增详情页

--- api/api.video.js
+++ api/api.video.js
@@ -1,7 +1,22 @@
 export default {
-    getTopMVs: {
+    getTopMVs: { // 获取mv列表
         method: 'GET',
         isMock: false,
         url: '/top/mv'
+    },
+    getMVUrl: { // 获取mv地址
+        method: 'GET',
+        isMock: false,
+        url: '/mv/url'
+    },
+    getMVDetail: { // 获取mv数据
+        method: 'GET',
+        isMock: false,
+        url: '/mv/detail'
+    },
+    getRelatedVideo: { // 获取相关视频
+        method: 'GET',
+        isMock: false,
+        url: '/related/allvideo'
     }
 }
--- pages/video/detail/index.js
+++ pages/video/detail/index.js
@@ -1,16 +1,36 @@
 // pages/video/detail/index.js
+import htRequest from '../../../utils/http'
+
 Page({
     /**
      * 页面的初始数据
      */
     data: {
-
+        mvURLInfo: {},
+        mvDetail: {},
+        relatedVideos: []
     },
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad(options) {
         const { id } = options
-        console.log(id);
+        this.getPageData(id)
+    },
+    /**
+     * @description 获取页面的数据
+     * @param {string} id
+     * @returns
+     */
+    getPageData(id) {
+        htRequest.run('getMVUrl', { id }).then((res) => {
+            this.setData({ mvURLInfo: res.data })
+        })
+        htRequest.run('getMVDetail', { mvid: id }).then((res) => {
+            this.setData({ mvDetail: res.data })
+        })
+        htRequest.run('getRelatedVideo', { id }).then((res) => {
+            this.setData({ relatedVideos: res.data })
+        })
     }
 })
